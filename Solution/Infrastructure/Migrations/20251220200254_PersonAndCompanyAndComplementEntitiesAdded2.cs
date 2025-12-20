84277e4139f50b277b57584153f526e422123bb1
leonardolucusi <lucusihiu@gmail.com>
2025-12-20 17:05:06 -0300

    feat: succefull migration of company, complement and person in customer schema

---
+++ Solution/Infrastructure/Migrations/20251220200254_PersonAndCompanyAndComplementEntitiesAdded2.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonAndCompanyAndComplementEntitiesAdded2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "customer");
+
+            migrationBuilder.CreateTable(
+                name: "company",
+                schema: "customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerType = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    FantasyName = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    LegalName = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    Cnpj = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    StateRegistration = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "person",
+                schema: "customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerType = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Cpf = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    Rg = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "complement",
+                schema: "customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerId = table.Column<int>(type: "integer", nullable: false),
+                    Address = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    AddressComplement = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    District = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Country = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    State = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    FederalUnit = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    ZipCode = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    Telephone = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Cellphone = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ContactName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_complement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_complement_company_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "customer",
+                        principalTable: "company",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_complement_person_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "customer",
+                        principalTable: "person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_complement_CustomerId",
+                schema: "customer",
+                table: "complement",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "complement",
+                schema: "customer");
+
+            migrationBuilder.DropTable(
+                name: "company",
+                schema: "customer");
+
+            migrationBuilder.DropTable(
+                name: "person",
+                schema: "customer");
+        }
+    }
+}
